e4a27f86f11d76f5c4b98c02b12eeca83218d8a5
Alvarado-Majo <mariajosealvarado235@gmail.com>
2025-11-01 15:01:48 -0600

    Creacion de clases Categoria y Tenis

---
+++ ProyectoTenis/src/main/java/com/ProyectoTenis/demo/domain/Tenis.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.ProyectoTenis.demo.domain;
+
+/**
+ *
+ * @author maria
+ */
+public class Tenis {
+
+}
